8a506960d8bf7dd31ecd12ee8d2349f1cbe3da61
Nadira Maysa Dyandra <nadira.maysa@gmail.com>
2022-09-15 14:21:03 +0700

    add xml dan json

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 data_barang_wishlist = BarangWishlist.objects.all()
@@ -11,3 +13,15 @@ context = {
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
 
+# Tutorial Lab02
+data = BarangWishlist.objects.all()
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
